e6fafac330217223d3d41847f9e35ecc35543f11
Christer <christer@swedakonsult.com>
2016-10-09 08:01:15 -0700

    Moved code into a function

--- week1/python/max_pairwise_product.py
+++ week1/python/max_pairwise_product.py
@@ -3,11 +3,15 @@ n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
-result = 0
+def pairwiseProduct():
+    result = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if a[i]*a[j] > result:
+                result = a[i]*a[j]
 
+    return result
+
+result = pairwiseProduct()
 print(result)
