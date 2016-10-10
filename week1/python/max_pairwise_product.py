b2fbb4af4af99a62ed47593e023455cebb56d25f
Christer <christer@swedakonsult.com>
2016-10-09 19:55:07 -0700

    Added fast method

--- week1/python/max_pairwise_product.py
+++ week1/python/max_pairwise_product.py
@@ -10,13 +10,27 @@ def pairwiseProduct(size, numbers):
 
     return result
 
+def pairwiseProductFast1(size, numbers):
+    max_index = -1
+    for i in range(0, size):
+        if max_index == -1 or numbers[i] > numbers[max_index]:
+            max_index = i
+
+    max_index2 = -1
+    for j in range(0, size):
+        if (numbers[j] != numbers[max_index]) and (max_index2 == -1 or numbers[j] > numbers[max_index2]):
+            max_index2 = j
+
+    return numbers[max_index] * numbers[max_index2]
 
 def main():
     n = int(input())
     a = [int(x) for x in input().split()]
     assert(len(a) == n)
 
-    result = pairwiseProduct(n, a)
+    #result = pairwiseProduct(n, a)
+    #print(result)
+    result = pairwiseProductFast1(n, a)
     print(result)
 
 main()
