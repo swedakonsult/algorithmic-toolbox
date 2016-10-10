f67cda2e2ab68ed20f4a44c7915f9ba8af165102
Christer <christer@swedakonsult.com>
2016-10-09 23:32:24 -0700

    Week 2: last digit
    * Added initial fast method
    * Added test that verifies that the fast method matches the old

--- week2/fibonacci_last_digit/fibonacci_last_digit.py
+++ week2/fibonacci_last_digit/fibonacci_last_digit.py
@@ -6,14 +6,66 @@ def get_fibonacci_last_digit_naive(n):
         return n
 
     previous = 0
-    current  = 1
+    current = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
 
     return current % 10
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+
+def calc_fibFast(n, lst):
+    if n <= 1:
+        return n
+
+    if lst[n] > 0:
+        #print('hit:', n)
+        return lst[n]
+
+    r = calc_fibFast(n - 1, lst) + calc_fibFast(n - 2, lst)
+    lst[n] = r
+    return r
+
+
+def get_fibonacci_last_digit_fast(n):
+    if n <= 1:
+        return n
+
+    #lst = [0 for i in range(10000000)]
+    #return (calc_fibFast(n-2, lst) + calc_fibFast(n-1, lst)) % 10
+    minusone = 1
+    minustwo = 0
+
+    for _ in range(n - 2):
+        minustwo, minusone = minusone, minustwo + minusone
+
+    return (minusone + minustwo) % 10
+
+
+def stressTest():
+    import random
+
+    while True:
+        n = random.randint(0, 100000)
+        print(n)
+
+        result = get_fibonacci_last_digit_naive(n)
+        result2 = get_fibonacci_last_digit_fast(n)
+        if result != result2:
+            print("Wrong answer:", result, " ", result2)
+            break;
+        else:
+            print('OK')
+
+
+def main():
+    input_n = input()
+    if 'stress' in input_n:
+        stressTest()
+        return
+
+    n = int(input_n)
+    #print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit_fast(n))
+
+main()
