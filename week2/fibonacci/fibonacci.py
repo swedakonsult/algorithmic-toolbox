085445ed6805d58baadcf3bf1252d8655e21e9ac
Christer <christer@swedakonsult.com>
2016-10-09 22:49:58 -0700

    Week 2 fast basic fibonacci

--- week2/fibonacci/fibonacci.py
+++ week2/fibonacci/fibonacci.py
@@ -1,9 +1,29 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+
+def calc_fibFast(n, lst):
+    if n <= 1:
+        return n
+
+    if lst[n] > 0:
+        #print('hit:', n)
+        return lst[n]
+
+    r = calc_fibFast(n - 1, lst) + calc_fibFast(n - 2, lst)
+    lst[n] = r
+    return r
+
+
+def main():
+    n = int(input())
+
+    # expecting range of n: 0 <= n <= 45
+    lst = [0 for i in range(46)]
+    print(calc_fibFast(n, lst))
+
+main()
