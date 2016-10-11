7b3aa2b7df007712964fc211ef413c920c8ecf11
Christer <christer@swedakonsult.com>
2016-10-10 23:11:19 -0700

    Week 2 - fibonacci huge pisano
    * Fixed the fast so that it matches most cases to the correct answer
     when comparing to naive
    * Got the largest example numbers to calculate properly

--- week2/fibonacci_huge/fibonacci_huge.py
+++ week2/fibonacci_huge/fibonacci_huge.py
@@ -8,7 +8,7 @@ def get_fibonacci_huge_naive(n, m):
         return n
 
     previous = 0
-    current  = 1
+    current = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
@@ -16,11 +16,68 @@ def get_fibonacci_huge_naive(n, m):
     return current % m
 
 
-def compare_n_m(n, m):
+def find_pisano_period(n, m, debug=False):
+    if m == 2:
+        return 3, 0
+    elif m == 3:
+        return 8, 0
+    elif m == 4:
+        return 6, 0
+    elif m == 5:
+        return 20, 0
+
+# find the '01' pattern, iterating from n = 0
+    previous = 0
+    current = 1
+
+    for i in range(n - 1):
+        previous, current = current, previous + current
+        mod_p = previous % m
+        mod_c = current % m
+        if debug:
+            print(i, end=',')
+        if mod_p == 0 and mod_c == 1:
+            # found the period of Pisano
+            print('')
+            return i + 1, 0
+
+    print('')
+    return 0, mod_c
+
+
+def get_fibonacci_huge_fast(n, m, debug=False):
+    if n <= 1:
+        return n
+
+    m_period, res = find_pisano_period(n, m, debug)
+    if debug:
+        print('period', m_period)
+    if m_period == 0:
+        if debug:
+            print('full iteration')
+        return res
+    n_snubbed = n % m_period
+    if debug:
+        print('period remainder', n_snubbed)
+    if n_snubbed == 0:
+        return 0
+    elif n_snubbed == 1:
+        return 1
+
+    previous = 0
+    current = 1
+
+    for _ in range(n_snubbed - 1):
+        previous, current = current, previous + current
+
+    return current % m
+
+
+def compare_n_m(n, m, debug=False):
     print(n, m)
 
     result = get_fibonacci_huge_naive(n, m)
-    result2 = 0 # lcm_fast(a, b)
+    result2 = get_fibonacci_huge_fast(n, m, debug)
     if result != result2:
         print("Wrong answer:", result, " ", result2)
         return False
@@ -29,18 +86,18 @@ def compare_n_m(n, m):
         return True
 
 
-def stress_test(input_n=''):
+def stress_test(input_n='', debug=False):
     if input_n != '':
         n, m = map(int, input_n.split())
-        compare_n_m(n, m)
+        compare_n_m(n, m, debug)
         return
 
     import random
 
     while True:
-        n = random.randint(0, 1000)
-        m = random.randint(0, 1000)
-        if not compare_n_m(n, m):
+        n = random.randint(1, 1000)
+        m = random.randint(2, 1000)
+        if not compare_n_m(n, m, debug):
             break
 
 
@@ -49,7 +106,7 @@ def main(input_n, naive=False, debug=False):
     if naive:
         print(get_fibonacci_huge_naive(n, m))
     else:
-        print('none', debug) # print(lcm_fast(n, m, debug))
+        print(get_fibonacci_huge_fast(n, m, debug))
 
 
 if __name__ == '__main__':
@@ -60,6 +117,10 @@ if __name__ == '__main__':
         else:
             stress_test(input_r[7:])
     elif 'debug' in input_r:
-        main(input_r[:-6], False, True)
+        if len(input_r) < 7:
+            stress_test('', True)
+        else:
+            stress_test(input_r[6:], True)
+        # main(input_r[:-6], False, True)
     else:
-        main(input_r, True)
+        main(input_r)
