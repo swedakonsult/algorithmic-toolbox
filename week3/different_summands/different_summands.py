b68c54062d29a58a54fb65b1b3e7e15ca71fe420
Christer <christer@swedakonsult.com>
2016-10-23 20:50:33 -0700

    Week 3 - different summands
    * implemented logic
    * added extra tests

--- week3/different_summands/different_summands.py
+++ week3/different_summands/different_summands.py
@@ -1,10 +1,44 @@
 # Uses python3
 import sys
 
-def optimal_summands(n):
+
+def optimal_summands(n, debug=False):
+    if n == 0:
+        return 0
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [2]
     summands = []
-    #write your code here
+    sum = 0
+    rem = n
+    current = 1
+    summands.append(current)
+    sum += current
+    rem -= current
+    # iterate
+    while rem > 0:
+        current += 1
+        next_value = current + 1
+        if rem == current:
+            summands.append(current)
+            break
+        if rem - current < next_value:
+            if debug:
+                print('continue', ['rem', rem], ['current', current], ['next', next])
+            continue
+        if debug:
+            print('iteration', ['rem', rem], ['current', current], ['next', next_value], summands)
+        sum += current
+        if rem - current < next_value:
+            continue
+        rem -= current
+        summands.append(current)
+        if sum == n:
+            break
+
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
